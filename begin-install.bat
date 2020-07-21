80130976f96d5930d48e7be5cf398e84429366f5
ankitp1288 <ankitp1288@gmail.com>
2020-07-21 19:33:29 -0400

    Add files via upload

---
+++ begin-install.bat
@@ -0,0 +1,3 @@
+REM Install Internet Information Server (IIS).
+C:\Users\Administrator\powershell.exe -Command Import-Module -Name ServerManager
+C:\Users\Administrator\powershell.exe -Command Install-WindowsFeature Web-Server
